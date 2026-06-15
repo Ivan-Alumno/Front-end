fb1dff087f14f53a0831c0dee34bd740cd9e9de7
Ivan-Alumno <ivan.villagra03@inacapmail.cl>
2026-06-15 04:40:04 -0400

    Ningun cambio

--- React/Bibidex/src/components/Benefits.jsx
+++ React/Bibidex/src/components/Benefits.jsx
@@ -9,14 +9,12 @@ export default function Benefits() {
             <ul>
 
                 <li>
-
                     <h3>Fácil de usar</h3>
                     <p>Interfaz intuitiva para todos los usuarios.</p>
 
                 </li>
 
                 <li>
-
                     <h3>Soporte 24/7</h3>
                     <p>Estamos disponibles para ayudarte en cualquier momento.</p>
 
